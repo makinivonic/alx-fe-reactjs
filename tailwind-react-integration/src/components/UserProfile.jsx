8744fc9c7638fc5722e0f241265297e563f3a30b
makinivonic <makinivonic@gmail.com>
2025-03-19 12:43:49 +0300

    Fixed md:max-w-sm as per review feedback

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto my-10 rounded-lg shadow-lg">
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm lg:max-w-lg mx-auto my-10 rounded-lg shadow-lg">
       <img
         className="w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto"
         src="/vite.svg"
